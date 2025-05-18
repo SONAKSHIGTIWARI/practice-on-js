05dca92474c3ae2e6ef3b15505a0edc73fbcdb6e
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-05-18 22:00:04 +0530

    advanced js

---
+++ exception.js
@@ -0,0 +1,50 @@
+// exception handling
+
+// console.log(10 / 0)
+
+// try catch block
+
+// try {
+
+//     // the code where exception might apear
+
+//     let a = 10
+//     let b = 5
+
+//     if(b == 0){
+//         throw("denominator cannot be '0'")
+//     }
+
+//     let answer = a / b
+
+//     console.log(answer)
+
+// } catch (e) {
+//     // the resolving to that code 
+//     console.log("an exception occured : ",e)
+// }
+
+// exception pre-defined / user-defined
+
+// try {
+
+//     consolelog("data")
+
+//     let a = 12
+
+// } catch (e) {
+//     console.log("an exception occured ", e)
+//     console.error("an exception occured ", e)
+//     console.warn("an exception occured ", e)
+//     console.info("an exception occured ", e)
+// }
+
+// promises and async programming
+
+// promises
+// pending resolved rejected
+// handel all 3 state of promise
+
+// whatsapp
+
+// sender -> message -> user
