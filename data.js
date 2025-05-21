fd9aa219408456096f0869a74a9679ad7b450f0e
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-05-21 22:54:25 +0530

    myscripts

---
+++ data.js
@@ -0,0 +1,24 @@
+
+// for each
+//let rr=[1,2,3,4,5];
+//rr.forEach( loop=>{
+//    console.log(loop);
+//})
+
+//let pr=[1,2,3,4,5];
+//pr.forEach(function(loop,a){//a->index
+//    console.log(a,loop);
+//})
+
+
+let employee={name:"employee1",age:25,experiance:4,salary:80000};
+for (let key in employee) {
+    console.log(key,employee[key]);
+    
+}
+
+
+
+
+
+
