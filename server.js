0a1fb0f725c05cbfe64972c863f10178ca53c8fd
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-07-26 23:38:28 +0530

    server.js

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import ejs from "ejs"
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { router } from "./routers/router.js";
 
-import { router } from "./routers/router.js"
+dotenv.config();
 
-dotenv.config({ path: "./config.env" })
+const app = express();
+const port = process.env.PORT || 4441;
 
-const port = process.env.PORT || 4441
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const app = express()
+app.set("view engine", "ejs");
 
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(express.static("public"))
-
-app.set("view engine", "ejs")
-
-app.use(router)
+app.use(router);
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port} ! | http://localhost:${port}.`)
-})
+  console.log(`✅ Server is running on port ${port} | http://localhost:${port}`);
+});
